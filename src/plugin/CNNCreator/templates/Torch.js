d97f54746ed8970ab3d96a2898472501cc7c4a6f
JimnyCricket <JimmyMWhitaker@gmail.com>
2015-09-02 23:11:04 -0500

    Added support for some Torch Layers.

--- src/plugin/CNNCreator/templates/Torch.js
+++ src/plugin/CNNCreator/templates/Torch.js
@@ -9,7 +9,25 @@ define([], function() {
 
     // Convolution layer
     blockMap.Convolution = 'CNN:add(nn.SpatialConvolution(3, '+
-        '{{= num_output }}, {{= kernel_size }}, {{= kernel_size }}, {{= stride }}, {{= stride }}, {{= pad }}, {{= pad }})';
+        '{{= num_output }}, {{= kernel_size }}, {{= kernel_size }}, {{= stride }}, {{= stride }}, {{= pad }}, {{= pad }}))';
+
+    blockMap.Dropout = 'CNN:add(nn.Dropout('+
+        '{{= dropout_ratio }}))';
+
+    blockMap.Flatten = 'CNN:add(nn.View())';
+
+    blockMap.Pooling = 'CNN:add(nn.SpatialMaxPooling('+
+        '{{= kernel_size }}, {{= kernel_size }}, {{= stride }}, {{= stride }}))';
+
+    // TODO Local response normalization does not exist in nn package (exists in cudnn)
+
+    blockMap.InnerProduct = 'CNN:add(nn.Linear('+
+        '{{= num_output }}, {{= num_output }}))'; // TODO need number of inputs
+
+    blockMap.ReLU = 'CNN:add(nn.ReLU(true))';
+
+    blockMap.Softmax = 'CNN:add(nn.Softmax())';
+
 
     for (var key in blockMap) {
         blockMap[key] += '\n';
